18a3ea618b989c8e09d4ee44324b1df98222644b
warriorx72 <39719764+warriorx72@users.noreply.github.com>
2021-02-06 22:05:02 -0600

    Hello world

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.your-class').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
